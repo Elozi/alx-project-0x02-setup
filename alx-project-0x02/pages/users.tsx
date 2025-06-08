a57bc2e980a8091353eaa1e27e188045e54d6a7d
Abigail Ogbogo <101398676+Elozi@users.noreply.github.com>
2025-06-08 17:31:30 +0100

    Update users.tsx

--- alx-project-0x02/pages/users.tsx
+++ alx-project-0x02/pages/users.tsx
@@ -1,28 +1,14 @@
-import React, { useEffect, useState } from "react";
+// pages/users.tsx
+import React from "react";
 import Header from "@/components/layout/Header";
 import UserCard from "@/components/common/UserCard";
 import { UserProps } from "@/interfaces";
 
-const Users = () => {
-  const [users, setUsers] = useState<UserProps[]>([]);
+interface UsersPageProps {
+  users: UserProps[];
+}
 
-  useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/users")
-      .then((res) => res.json())
-      .then((data) =>
-        setUsers(
-          data.map((user: any) => ({
-            name: user.name,
-            email: user.email,
-            address: {
-              street: user.address.street,
-              city: user.address.city,
-            },
-          }))
-        )
-      );
-  }, []);
-
+const Users: React.FC<UsersPageProps> = ({ users }) => {
   return (
     <>
       <Header />
@@ -34,6 +20,27 @@ const Users = () => {
       </main>
     </>
   );
+};
+
+// ✅ Add static site generation
+export const getStaticProps = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await res.json();
+
+  const users: UserProps[] = data.map((user: any) => ({
+    name: user.name,
+    email: user.email,
+    address: {
+      street: user.address.street,
+      city: user.address.city,
+    },
+  }));
+
+  return {
+    props: {
+      users,
+    },
+  };
 };
 
 export default Users;
